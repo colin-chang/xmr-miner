a5a575e5ce2b4fe9a05bdefb6d4920c073b2d8e3
Colin Chang <zhangcheng5468@gmail.com>
2019-12-23 10:47:10 +0800

    update default worker name

--- xmrig-5.3.0-linux/start.sh
+++ xmrig-5.3.0-linux/start.sh
@@ -1,1 +1,1 @@
-./xmrig -o xmr.f2pool.com:13531 -u 83j14db8zzaVarmQBmVXHZBnre8HKEi8QFh5p9fWnyzobSADaujKnwxN6ccEiFeLFXgGAsj2YvsTddJVQ3mukVg53fGUk77.worker_01 -p x -k --donate-level 1
+./xmrig -o xmr.f2pool.com:13531 -u 83j14db8zzaVarmQBmVXHZBnre8HKEi8QFh5p9fWnyzobSADaujKnwxN6ccEiFeLFXgGAsj2YvsTddJVQ3mukVg53fGUk77.colin -p x -k --donate-level 1
